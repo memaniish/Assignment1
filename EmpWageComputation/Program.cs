0262933e8271df6c741714cdeb742ca0e4efe218
Manish <krmanish2197@gmail.com>
2022-01-23 20:48:12 +0530

    First Commit

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empolyee Wage Computation Program");
+            //EmpAttendance empattendance = new EmpAttendance();
+            EmpAttendance.GetMonthlyEmpWage();
+            Console.ReadLine();
+        }
+    }
+}
